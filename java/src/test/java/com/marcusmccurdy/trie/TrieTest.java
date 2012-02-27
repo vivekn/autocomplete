4e033e8ca31639da62b2b9ca08e1e8cba094e69c
Marcus McCurdy <marcus.mccurdy@gmail.com>
2012-02-27 11:02:26 -0500

    improved tests and removed @BeforeClass method

--- java/src/test/java/com/marcusmccurdy/trie/TrieTest.java
+++ java/src/test/java/com/marcusmccurdy/trie/TrieTest.java
@@ -1,13 +1,9 @@
 package com.marcusmccurdy.trie;
 
-import java.io.*;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
@@ -15,18 +11,7 @@ import static org.junit.Assert.*;
  */
 public class TrieTest {
 
-    private static final List<String> words = new ArrayList<String>();
     private Trie root;
-    
-    @BeforeClass
-    public static void setUpClass() throws IOException {
-        final BufferedReader reader =
-                new BufferedReader(new InputStreamReader(TrieTest.class.getResourceAsStream("/words.txt")));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            words.add(line);
-        }
-    }
 
     @Before
     public void setUp() {
@@ -47,14 +32,19 @@ public class TrieTest {
      */
     @Test
     public void testInsert() {
-        String test = "test";
-        root.insert(test);
+        root.insert("test");
         assertEquals(1, root.children.size());
+        root.insert("basic");
+        assertEquals(2, root.children.size());
+        assertEquals(1, root.children.get('b').children.size());
+        assertEquals(1, root.children.get('t').children.size());
     }
 
-    /**
-     * Test of find method, of class Trie.
-     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testInsertNull() {
+        root.insert(null);
+    }
+
     @Test
     public void testFind() {
         root.insert("hello");
@@ -65,17 +55,16 @@ public class TrieTest {
         assertEquals("test", root.find("test"));
     }
 
-    /**
-     * Test of autoComplete method, of class Trie.
-     */
     @Test
     public void testAutoComplete() {
         root.insert("test");
         root.insert("tea");
         root.insert("tell");
+        root.insert("zebra");
         Collection<String> results = root.autoComplete("te");
         assertTrue(results.contains("test"));
         assertTrue(results.contains("tea"));
         assertTrue(results.contains("tell"));
+        assertFalse(results.contains("zebra"));
     }
 }
