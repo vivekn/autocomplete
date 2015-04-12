d0cb1f91602fba526a619c24b88c112832c20861
Colin Dean <git@cad.cx>
2015-04-12 01:20:27 -0400

    scala: Renames fixture property to improve readability

--- scala/src/test/scala/TrieTest.scala
+++ scala/src/test/scala/TrieTest.scala
@@ -6,27 +6,27 @@ class TrieTest extends WordSpec with Matchers {
 
   "The Trie" should {
     "insert a single character string" in {
-      val f = trieWithInsertions(List("f"))
+      val trie = trieWithInsertions(List("f")).theTrie
 
-      f.trie.children should have size 1
-      f.trie.children.head._2.value.get should be("f")
+      trie.children should have size 1
+      trie.children.head._2.value.get should be("f")
     }
 
     "insert a two single character strings" in {
-      val f = trieWithInsertions(List("f","z"))
+      val trie = trieWithInsertions(List("f","z")).theTrie
 
-      f.trie.children should have size 2
-      f.trie.children.head._2.value.get should be ("f")
-      f.trie.children.tail.head._2.value.get should be ("z")
+      trie.children should have size 2
+      trie.children.head._2.value.get should be ("f")
+      trie.children.tail.head._2.value.get should be ("z")
     }
 
     "insert one multicharacter string" in {
-      val f = trieWithInsertions(List("what"))
+      val trie = trieWithInsertions(List("what")).theTrie
 
-      f.trie.children should have size 1
-      f.trie.children.head._2.value should be (None)
+      trie.children should have size 1
+      trie.children.head._2.value should be (None)
 
-      val w: (Char, Trie) = f.trie.children.head
+      val w: (Char, Trie) = trie.children.head
       val h: (Char, Trie) = w._2.children.head
       val a: (Char, Trie) = h._2.children.head
       val t: (Char, Trie) = a._2.children.head
@@ -42,11 +42,11 @@ class TrieTest extends WordSpec with Matchers {
     }
 
     "insert two multicharacter strings what begin with the same prefix" in {
-      val f = trieWithInsertions(List("what","who"))
+      val trie = trieWithInsertions(List("what","who")).theTrie
 
-      f.trie.children should have size 1
+      trie.children should have size 1
 
-      val w: (Char, Trie) = f.trie.children.head
+      val w: (Char, Trie) = trie.children.head
       val h: (Char, Trie) = w._2.children.head
       val a: (Char, Trie) = h._2.children.head
       val t: (Char, Trie) = a._2.children.head
@@ -65,79 +65,79 @@ class TrieTest extends WordSpec with Matchers {
     }
 
     "insert two multicharacter strings that do not have the same prefix" in {
-      val f = trieWithInsertions(List("what","fun"))
+      val trie = trieWithInsertions(List("what","fun")).theTrie
 
-      f.trie.children.keySet shouldEqual Set('f', 'w')
+      trie.children.keySet shouldEqual Set('f', 'w')
     }
 
     "contain a single character string inserted" in {
-      val f = trieWithInsertions(List("f"))
+      val trie = trieWithInsertions(List("f")).theTrie
 
-      f.trie.contains("f") should be (true)
+      trie.contains("f") should be (true)
     }
 
     "does not contain a string when nothing was inserted" in {
-      val f = trieWithInsertions()
+      val trie = trieWithInsertions().theTrie
 
-      f.trie.contains("a") should be (false)
+      trie.contains("a") should be (false)
     }
 
     "does not contain a single character string that was not inserted" in {
-      val f = trieWithInsertions(List("f"))
+      val trie = trieWithInsertions(List("f")).theTrie
 
-      f.trie.contains("a") should be (false)
+      trie.contains("a") should be (false)
     }
 
     "contain a single, multicharacter string inserted" in {
-      val f = trieWithInsertions(List("what"))
+      val trie = trieWithInsertions(List("what")).theTrie
 
-      f.trie.contains("what") should be (true)
+      trie.contains("what") should be (true)
     }
 
     "contain both of two multicharacter strings inserted" in {
       val words = List("who", "what")
-      val f = trieWithInsertions(words)
+      val trie = trieWithInsertions(words).theTrie
 
-      words.foreach { word => f.trie.contains(word) should be (true) }
+      words.foreach { word => trie.contains(word) should be (true) }
     }
 
     "become a nicely formatted string" in {
-      val f = trieWithInsertions(List("hello"))
+      val trie = trieWithInsertions(List("hello")).theTrie
 
-      f.trie.toString shouldEqual "{h: {e: {l: {l: {o: 'hello'}}}}}"
+      trie.toString shouldEqual "{h: {e: {l: {l: {o: 'hello'}}}}}"
     }
 
     "be readable when there are multiple words" in {
-      val f = trieWithInsertions(List("who", "where", "how"))
+      val trie = trieWithInsertions(List("who", "where", "how")).theTrie
 
-      f.trie.toString shouldEqual "{w: {h: {o: 'who'},{e: {r: {e: 'where'}}}}},{h: {o: {w: 'how'}}}"
+      trie.toString shouldEqual "{w: {h: {o: 'who'},{e: {r: {e: 'where'}}}}},{h: {o: {w: 'how'}}}"
     }
 
     "provide a list of all values inserted" in {
       val words = List("who", "where", "how")
-      val f = trieWithInsertions(words)
+      val trie = trieWithInsertions(words).theTrie
 
-      f.trie.allPrefixes should contain theSameElementsAs words
+      trie.allPrefixes should contain theSameElementsAs words
     }
 
     "provide a list of values that begin with a prefix" in {
       val words = List("who", "where", "how")
-      val f = trieWithInsertions(words)
+      val trie = trieWithInsertions(words).theTrie
 
-      f.trie.autocomplete("wh") should contain theSameElementsAs words.takeWhile(_.startsWith("wh"))
+      trie.autocomplete("wh") should contain theSameElementsAs words.takeWhile(_.startsWith("wh"))
     }
 
     "have only one entry when multiples of the same word are inserted" in {
       val words = List("who", "where", "who")
-      val f = trieWithInsertions(words)
+      val trie = trieWithInsertions(words).theTrie
 
-      f.trie.allPrefixes should have size 2
+      trie.allPrefixes should have size 2
     }
   }
 
   def trieWithInsertions(toInsert: List[String] = List.empty) = {
     new {
-      val trie = toInsert.foldLeft(new Trie)( (trie, word) => {trie.insert(word); trie})
+      val theTrie = toInsert.foldLeft(new Trie)( (trie, word) => {trie.insert(word); trie})
     }
   }
 
